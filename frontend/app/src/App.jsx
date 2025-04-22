30a6f50549c7048f1df0f056ad02fb983756d981
Pawelonsky <p_nowak10@wp.pl>
2025-04-22 20:24:42 +0200

    frontend

---
+++ frontend/app/src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Login } from "./components/Login"
+import { Registration } from "./components/Registration"
+import { ResetPassword } from "./components/ResetPassword"
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/registration" element={<Registration />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/app/src/components/Login.jsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+import "./login.css"
+import { Link } from 'react-router-dom'
+
+
+export const Login = () => {
+    
+ const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+ })
+
+ const [errors, setErrors] = useState({})
+
+ const handleChange = e => {
+    const {name, value} = e.target
+    setFormData({
+        ...formData, [name] : value
+    })
+ }
+
+ const handleSubmit = e => {
+    e.preventDefault()
+    const validationErrors = {}
+
+    if(!formData.email.trim()) {
+        validationErrors.email = "Wpisz prawidłowy adres e-mail"
+    } else if(!/\S+@\S+\.\S+/.test(formData.email)){
+        validationErrors.email = "Wpisz prawidłowy adres e-mail"
+    }
+
+    if(!formData.password.trim()) {
+        validationErrors.password = "To pole jest obowiązkowe"
+    } else if(formData.password.length < 6){
+        validationErrors.password = "To pole jest obowiązkowe"
+    }
+
+    setErrors(validationErrors)
+    if(Object.keys(validationErrors).length === 0) {
+        alert("elo burdelo")
+    }
+ }
+
+    return (
+        <>
+            <div className="wrapper">
+                <form onSubmit={handleChange} action="">
+                    <h2>Zaloguj się</h2>
+                    <div className="form-box">
+                        <label htmlFor="email">Email</label>
+                        <input 
+                            type="email" 
+                            name="email"
+                            placeholder="Twój adres email"
+                            onChange={handleChange}
+                            />
+                            {errors.email && <span>{errors.email}</span>}
+                           
+                    </div>
+                    <div className="form-box">
+                        <label htmlFor="password">Hasło</label>
+                        <input 
+                            type="password"
+                            name="password" 
+                            placeholder="Hasło"
+                            onChange={handleChange}
+                            />
+                        {errors.password && <span>{errors.password}</span>}
+                    </div>
+                    <Link to="/reset-password" className="link link__password"><span>Nie pamiętasz hasła?</span></Link>
+                    <button onClick={handleSubmit} type="submit" className="login__button">Zaloguj się</button>
+                    <div className="register">Nie masz konta? <Link to="/registration" className="link">Zarejestruj się</Link></div>
+                </form>
+            </div>
+        </>
+    )
+}
+
