98aa54ec2a31feb11a3fd02741cec1486a879a18
KarolOG <k.ogorzalek1@gmail.com>
2025-05-04 21:58:39 +0200

    CORS

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@ import app.api.user as APIuser
 import app.api.group as APIgroup
 from  app.db import Base, engine
 import app.api.post as APIpost
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
@@ -10,6 +11,19 @@ app.include_router(APIuser.router)
 app.include_router(APIgroup.router)
 app.include_router(APIpost.router)
 Base.metadata.create_all(bind=engine)
+
+origins = [
+    "http://localhost:5173",
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
